f661e43754e654e9284e86786c161a1f8a23b054
Evgenij Mukhanov <evgenij.mukhanov@gmail.com>
2023-02-15 16:48:20 +0300

    shared/Modal apply

--- shared/src/App.tsx
+++ shared/src/App.tsx
@@ -5,12 +5,14 @@ import { Container } from './containers/index'
 
 import './index.css'
 import './styles/variable.css'
+import { Modal } from "./modals/Modal/Modal";
 
 const App = () => (
   <Container>
     <Button>Button</Button>
     <Input/>
     <Text title="Text">Text</Text>
+    <Modal></Modal>
   </Container>
 );
 
